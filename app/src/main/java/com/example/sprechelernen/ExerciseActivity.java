9d3be7ef597f22205dee429eed4976921e11002d
xaju5 <xajubdev@gmail.com>
2022-03-29 23:50:33 +0200

    New File selection

--- app/src/main/java/com/example/sprechelernen/ExerciseActivity.java
+++ app/src/main/java/com/example/sprechelernen/ExerciseActivity.java
@@ -3,11 +3,13 @@ package com.example.sprechelernen;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,46 +19,65 @@ public class ExerciseActivity extends AppCompatActivity {
 
     private TextView attemptsTV, questionTV, typeTV;
     private EditText answerET;
-    private String rightAnswer, fileName, questionStr, typeStr;
+    private String rightAnswer, filename, questionStr, typeStr;
     private String[] exerWords;
     private List<String[]> allWords;
     private int attemptsNum;
 
+    private String verbenfileName, nomenfileName, adjfileName;
+    private String PATH = "/data/data/com.example.sprechelernen/data/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
 
-        Bundle extras = getIntent().getExtras();
-        if (extras != null){
-            fileName = extras.getString("fileName");
-        }
-
-
         attemptsTV = findViewById(R.id.attemptstextView);
         questionTV = findViewById(R.id.questiontextView);
         typeTV = findViewById(R.id.typeTextView);
         answerET = findViewById(R.id.answereditText);
 
-        CSVReader csvReader = new CSVReader(ExerciseActivity.this,fileName);
-        try {
-            allWords = csvReader.readCSV();
-        } catch (IOException e){
-            e.printStackTrace();
-            Toast toastFileError = Toast.makeText(getApplicationContext(),"File reader error",Toast.LENGTH_SHORT);
-            toastFileError.show();
-
-            //Error array
-            String[] errorStr = {"ERROR","ERROR","ERROR"};
-            allWords = new ArrayList<String[]>();
-            allWords.add(errorStr);
-            allWords.add(errorStr);
+        int code = 0;
+        Bundle extras = getIntent().getExtras();
+        if (extras != null){
+            code = extras.getInt("code");
         }
 
-        chooseExerWords();
-        chooseQuestion();
-        updateTextView();
+        File file = null;
+        if(code == 1){
+            file = new File(getFilesDir()+"/data/verben.csv");
+        }
+        if(code == 2){
+            file = new File(getFilesDir()+"/data/nomen.csv");
+        }
+        if(code == 3){
+            file = new File(getFilesDir()+"/data/adjektiv.csv");
+        }
+
+        if(file.exists()){
+            CSVReader csvReader = new CSVReader(ExerciseActivity.this,file.getPath());
+            try {
+                allWords = csvReader.readCSV();
+            } catch (IOException e){
+                e.printStackTrace();
+                Toast toastFileError = Toast.makeText(getApplicationContext(),"File reader error",Toast.LENGTH_SHORT);
+                toastFileError.show();
+
+                //Error array
+                String[] errorStr = {"ERROR","ERROR","ERROR"};
+                allWords = new ArrayList<String[]>();
+                allWords.add(errorStr);
+                allWords.add(errorStr);
+            }
+        }
+        else{
+            Toast toastNotFile = Toast.makeText(getApplicationContext(),"File does not exist",Toast.LENGTH_SHORT);
+            toastNotFile.show();
+        }
+
+        //chooseExerWords();
+        //chooseQuestion();
+        //updateTextView();
     }
 
     private  void chooseExerWords(){
--- app/src/main/java/com/example/sprechelernen/MainActivity.java
+++ app/src/main/java/com/example/sprechelernen/MainActivity.java
@@ -2,40 +2,41 @@ package com.example.sprechelernen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
 
-    String verbenfileName, nomenfileName, adjfileName;
-    String PATH = "/data/data/com.example.sprechelernen/data/";
+    private int verbenCode = 1;
+    private int nomenCode = 2;
+    private int adjCode = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        verbenfileName = PATH + "verben.csv";
-        nomenfileName = PATH + "nomen.csv";
-        adjfileName = PATH + "adjektiv.csv";
     }
 
     public void goToVerb(View view){
         Intent i = new Intent(MainActivity.this,ExerciseActivity.class);
-        i.putExtra("fileName", verbenfileName);
+        i.putExtra("code", verbenCode);
         startActivity(i);
     }
 
     public void goToNomen(View view){
         Intent i = new Intent(MainActivity.this,ExerciseActivity.class);
-        i.putExtra("fileName", nomenfileName);
+        i.putExtra("code", nomenCode);
         startActivity(i);
     }
 
     public void goToAdj(View view){
         Intent i = new Intent(MainActivity.this,ExerciseActivity.class);
-        i.putExtra("fileName", adjfileName);
+        i.putExtra("code", adjCode);
         startActivity(i);
     }
 
